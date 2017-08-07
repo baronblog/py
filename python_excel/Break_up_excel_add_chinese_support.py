058b1416e56b0b871da16a02f982be75af8b2c6b
yangyang510 <11437823+yangyang510@users.noreply.github.com>
2017-08-07 11:34:23 +0800

    添加支持中文分割

---
+++ python_excel/Break_up_excel_add_chinese_support.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import xlrd
+
+#read_temple=pd.read_excel("C:/Users/Hymn/Desktop/read_temple.xlsx")
+#read_temple.to_excel("C:/Users/Hymn/Desktop/ceshi.xlsx")
+
+read_temple=pd.ExcelFile("C:/Users/Hymn/Desktop/read_temple.xlsx")
+read_temple_sheet1=read_temple.parse("Total_Bill")
+read_temple_sheet2=read_temple.parse("Bill_Detail")
+read_temple_sheet3=read_temple.parse("Shipped_Detail")
+
+company=[]
+for r in read_temple_sheet1['公司名'.decode("utf-8")]:
+    company.append(r)
+
+
+
+company_unique=list(set(company))
+
+
+
+for company_name in  company_unique:
+    read_temple_sheet1_filter1=read_temple_sheet1[read_temple_sheet1[u'公司名']==company_name]
+    read_temple_sheet2_filter2 = read_temple_sheet2[read_temple_sheet2['公司名'.decode("utf-8")] == company_name]
+    read_temple_sheet3_filter3 = read_temple_sheet3[read_temple_sheet3['公司名'.decode("utf-8")] == company_name]
+
+    writer = pd.ExcelWriter("C:/Users/Hymn/Desktop/template/"+company_name+".xlsx")
+    read_temple_sheet1_filter1.to_excel(writer,"Total_Bill",index=False)
+    read_temple_sheet2_filter2.to_excel(writer, "Bill_Detail",index=False)
+    read_temple_sheet3_filter3.to_excel(writer, "Shipped_Detail",index=False)
+    writer.save()
