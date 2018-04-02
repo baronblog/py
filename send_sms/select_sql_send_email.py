80e340f9e125228fc5475b6d33173978c8301b69
yangyang510 <11437823+yangyang510@users.noreply.github.com>
2018-04-02 09:26:59 +0800

    Update select_sql_send_email.py

--- send_sms/select_sql_send_email.py
+++ send_sms/select_sql_send_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-#python3.6
+#python3.5
 
 import pymysql
 import pandas as pd
@@ -12,9 +12,20 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 def connection(sql,dates):
-    conn = pymysql.connect(host='jumia-slave.cdhsfguj9gcoe.ap-fghsdft-1.rds.amazonaws.com', port=7866, user='user_lyang', passwd='dgsdfgssaWyXid7P1oGFog2vJ8b8', db='kilimall_kenya')
+    conn = pymysql.connect(host='fgshdfghsgf', port=8799, user='user_louis', passwd='g75ssaWgsdhfgdFog2vJ8b8', db='database')
     cursor = conn.cursor()
+    cursor.execute('SET time_zone = "+3:00"')
     effect_row = cursor.execute(sql,dates)
+    result=cursor.fetchall()
+    return result
+    conn.commit()
+    cursor.close()
+    conn.close()
+	
+def connections(sql):
+    conn = pymysql.connect(host='sgfhdsfe.cyvvadsgfhdsgfaws.com', port=3306, user='user_louis', passwd='g7dgeyfgygfoGFog2vJ8b8', db='database')
+    cursor = conn.cursor()
+    effect_row = cursor.execute(sql)
     result=cursor.fetchall()
     return result
     conn.commit()
@@ -23,6 +34,7 @@ def connection(sql,dates):
 
 
 sql1='''
+
 SELECT
  ooo.goods_id,
  ooo.goods_name,
@@ -35,8 +47,8 @@ SELECT
  gc1.gc_name,
  gc2.gc_name,
  ooo.paymenttime,
- ooo.leixing,
- ooo.goods_price
+ooo.leixing,
+ooo.goods_price
 FROM
  (
   SELECT
@@ -56,46 +68,42 @@ FROM
   ELSE
    FROM_UNIXTIME(o.payment_time, '%%Y%%m%%d')
   END AS paymenttime,
-  CASE o.logistics_type
- WHEN 1 THEN
-  'FBK'
- WHEN 2 THEN
-  'GS'
- WHEN 0 THEN
-  'DS'
- END AS leixing,
- SUM(og.goods_num) AS solditem
-FROM
- nc_order_goods og
-INNER JOIN nc_order o ON og.order_id = o.order_id
-INNER JOIN nc_goods gs ON og.goods_id = gs.goods_id
-INNER JOIN nc_goods_common common ON gs.goods_commonid = common.goods_commonid
-WHERE
- FROM_UNIXTIME(o.add_time, '%%Y%%m%%d') =%s
-AND common.goods_commonid IN (
- SELECT
-  gs.goods_commonid
+ CASE o.logistics_type WHEN 1 THEN 'FBK'
+WHEN 2 THEN 'GS'
+WHEN 0 THEN 'DS'
+END AS leixing,
+  SUM(og.goods_num) AS solditem
  FROM
-  nc_selected_goods nsgs
-INNER JOIN nc_goods gs ON nsgs.goods_id=gs.goods_id
-)
-GROUP BY
- og.goods_id,
- gs.gc_id_1,
- gs.gc_id_2,
- gs.gc_id_3,
- og.goods_name,
- og.store_id,
- og.goods_price,
- o.store_name,
- gs.goods_storage,
- paymenttime,
- leixing
+  nc_order_goods og
+ INNER JOIN nc_order o ON og.order_id = o.order_id
+ INNER JOIN nc_goods gs ON og.goods_id = gs.goods_id
+ WHERE
+FROM_UNIXTIME(o.add_time, '%%Y%%m%%d') =%s
+ GROUP BY
+  og.goods_id,
+   og.store_id,
+   o.store_name,
+   gs.gc_id_1,
+   gs.gc_id_2,
+   gs.gc_id_3,
+   og.goods_name,
+   og.goods_price,
+   gs.goods_storage,timess,paymenttime,leixing
  ) AS ooo
 INNER JOIN nc_goods_class gc ON ooo.gc_id_1 = gc.gc_id
 INNER JOIN nc_goods_class gc1 ON ooo.gc_id_2 = gc1.gc_id
 INNER JOIN nc_goods_class gc2 ON ooo.gc_id_3 = gc2.gc_id
 '''
+
+sql2='''
+ SELECT
+  nsgs.goods_id,
+	'Y'
+ FROM
+  nc_selected_goods nsgs
+  group by nsgs.goods_id
+'''
+
 def getYesterday():
     today=datetime.date.today()
     oneday=datetime.timedelta(days=1)
@@ -108,41 +116,55 @@ result_req=connection(sql1,today)
 result_list=list(result_req)
 result=pd.DataFrame(result_list)
 result.columns=['goods_id','goods_name','stock','solditem','storeid','store_name','addtime','l1','l2','l3','paymenttime','type','goods_price']
-a=result.to_excel("C:/Users/Hymn/Desktop/sheet.xlsx")
+result.to_excel("C:/Users/Hymn/Desktop/ceshi.xlsx",index=False)
 
-username = 'your email'
-password = 'your passwords'
+result_top=connections(sql2)
+result_top_list=list(result_top)
+result_selection=pd.DataFrame(result_top_list)
+result_selection.columns=['goods_id','Top Selection']
+result_selection.to_excel("C:/Users/Hymn/Desktop/top.xlsx",index=False)
+result_finally=pd.merge(result,result_selection,on="goods_id",how="left")
+result_finally.to_excel("C:/Users/Hymn/Desktop/yesterday_sold_data.xlsx",index=False)
+print("first file already")
+
+
+
+username = 'your send email'
+password = 'your send email passwords'
 sender = username
-receivers = ','.join(['receiver email'])
+receivers = ','.join(['your receivers email'])
 
 # 如名字所示： Multipart就是多个部分
 msg = MIMEMultipart()
-msg['Subject'] = 'Python mail Test'
+msg['Subject'] = today+" Sold Item Data"
 msg['From'] = sender
 msg['To'] = receivers
 
 # 下面是文字部分，也就是纯文本
-puretext = MIMEText('我是纯文本部分，')
+puretext = MIMEText("Hi,Echo, attachment file is "+ today +"sold item data, kindly check it, thanks")
 msg.attach(puretext)
-
+print("email content already")
 # 下面是附件部分 ，这里分为了好几个类型
 
-# 首先是xlsx类型的附件
-xlsxpart = MIMEApplication(open('sheet.xlsx', 'rb').read())
-xlsxpart.add_header('Content-Disposition', 'attachment', filename='sheet.xlsx')
+# 首先是xlsx类型的附件，全部销售数据
+xlsxpart = MIMEApplication(open("C:/Users/Hymn/Desktop/yesterday_sold_data.xlsx", 'rb').read())
+xlsxpart.add_header('Content-Disposition', 'attachment', filename='yesterday_sold_data.xlsx')
 msg.attach(xlsxpart)
-#如果需要多个附件，需要多个msg.attach
+print("first attachment file already")
 
 try:
+    #print("开始连接邮件服务器了")
     client = smtplib.SMTP()
     client.connect('imap.exmail.qq.com')
     client.login(username, password)
     client.sendmail(sender, receivers, msg.as_string())
     client.quit()
-    print('带有各种附件的邮件发送成功！')
+    print('today data already send.')
 except smtplib.SMTPRecipientsRefused:
     print('Recipient refused')
 except smtplib.SMTPAuthenticationError:
     print('Auth error')
 except smtplib.SMTPSenderRefused:
     print('Sender refused')
+except Exception as  e:
+    print(e)
