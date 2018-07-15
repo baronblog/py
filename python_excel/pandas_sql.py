d7b6de81f576b49aae498bced1f45cd2c2ec8ac2
yangyang510 <690566511@qq.com>
2018-07-15 15:36:11 +0800

    Update pandas_sql.py

--- python_excel/pandas_sql.py
+++ python_excel/pandas_sql.py
@@ -4,7 +4,9 @@ import tushare as ts
 import pymysql
 import time
 import random
+from sqlalchemy import create_engine
 from multiprocessing import Pool
+
 
 def connection(sql,*sqls):
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='kilimall5224910', db='tina',use_unicode=True, charset="utf8")
@@ -17,24 +19,35 @@ def connection(sql,*sqls):
     return result
 
 
-year=sorted([i for i in range(1999,2017,1)],reverse=True)
+year=[1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
 quarter=[1,2,3,4]
 print(year)
-def dealdata():
-    for r in year:
-        for i in quarter:
-            fund = pd.DataFrame(ts.fund_holdings(r, i))
-            datatime = str(r) + "0" + str(i)
-            #print(datatime)
-            #print(fund)
-            for index,x in fund.iterrows():
-                connection("INSERT INTO test VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",(x['code'],x['name'],x['date'],x['nums'],x['nlast'],x['count'],x['clast'],x['amount'],x['ratio']))
-                time.sleep(random.randint(3,10))
+def fundfunction(year,quarter):
+        for r in year:
+            for i in quarter:
+                fund = pd.DataFrame(ts.fund_holdings(r, i))
+                #time.sleep(1)
+                #print(fund)
+                print("得到结果"+str(fund.shape)+"行数据")
+                datatime = str(r) + "0" + str(i)
+                #print(datatime)
+                #print(fund)
+                with open("C:/Users/Hymn/Desktop/fund.txt","a+") as f:
+                    print("打开文件")
+                    for index,x in fund.iterrows():
+                        #print("开始循环")
+                        result="INSERT INTO fund_holding ("+"`code`,"+"`name`,"+"date,nums,nlast,count,clast,amount,ratio) VALUES ("+str(x['code'])+","+"'"+str(x['name'])+"'"+","+"'"+str(x['date'])+"'"+","+str(x['nums'])+","+str(x['nlast'])+","+str(x['count'])+","+str(x['clast'])+","+str(x['amount'])+","+str(x['ratio'])+");"+"\n"
+                        f.write(result)
+                        #print("写入一个结果")
 
-if __name__ == '__main__':
-    pool = Pool(4)
-    pool.apply_async(dealdata)
+#fundfunction(year,quarter)
+'''
+if __name__=='__main__':
+    pool=Pool(4)
+    pool.apply_async(fundfunction,(year,quarter))
     pool.close()
     pool.join()
+    '''
+
 
 
